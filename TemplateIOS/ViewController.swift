646f1faa78e2e161b6a7a6deccbd39063f7b7f5d
PaulNguyen <paul.nguyen.goldenowl@gmail.com>
2022-11-22 14:22:02 +0700

    init project

---
+++ TemplateIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TemplateIOS
+//
+//  Created by Golden Owl on 22/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
