c58831ae18432ce445f3e2005eef31e462955664
PaulNguyen <paul.nguyen.goldenowl@gmail.com>
2022-11-23 11:02:24 +0700

    init script

--- init/init.sh
+++ init/init.sh
@@ -1,11 +1,45 @@
+# exit when any command fails
+set -e
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: bash init.sh my.new.package" >&2
+    exit 2
+fi
+
+PACKAGE=$1
+APPNAME=$(echo $PACKAGE | rev | cut -d "." -f 1 | rev)
+
+# Rename package
+echo "Renaming packages to $PACKAGE name=$APPNAME"
+find . -type f -not -path "./.git/*" -not -path "./init/*" -exec sed -i.bak "s/com.ln.TemplateIOS/$PACKAGE/g" {} \;
+find . -type f -not -path "./.git/*" -not -path "./init/*" -exec sed -i.bak "s/TemplateIOS/$APPNAME/g" {} \;
+
+# Cleaning
+echo "Cleaning up"
+find . -name "*.bak" -type f -delete
+find . -name "*.DS_Store" -type f -delete
+
+# directories
+mv TemplateIOS ${APPNAME}
+mv TemplateIOS.xcodeproj ${APPNAME}.xcodeproj
+
+# Remove additional files
+echo "Removing additional files"
+rm -rf .git/
+
 # install
 brew install swiftformat
 npm install --global git-format-staged
 
 # pre-commit
-cp pre-commit .git/hooks/
+git init .
+cp init/pre-commit .git/hooks/
+chmod +x .git/hooks/pre-commit
 
 # remove new file comment
-mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates/Source
-cp -R Custom\ File.xctemplate ~/Library/Developer/Xcode/Templates/File\ Templates/Source
+# mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates/Source
+# cp -R init/Custom\ File.xctemplate ~/Library/Developer/Xcode/Templates/File\ Templates/Source
 
+rm -rf init
+
+echo "Done!"
