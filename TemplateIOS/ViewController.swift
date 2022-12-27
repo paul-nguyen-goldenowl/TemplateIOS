93ee17e5c3325a47cef35a6891e80144d021a5e5
paul-nguyen-goldenowl <paul.nguyen.goldenowl@gmail.com>
2022-12-27 14:18:02 +0700

    add lint

--- TemplateIOS/ViewController.swift
+++ TemplateIOS/ViewController.swift
@@ -5,5 +5,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .blue
+
+        if true {
+            print("adsfnalkdjfnklasmfksamdfkljadk;lfjnkldfsdfsdfdsfsdfasdnf;klasndflkansdfosdjn;flkdaslfnmlsd")
+        }
+
+        let aa = [1, 2, 4, 45, 5, 5, 5, 5]
     }
 }
