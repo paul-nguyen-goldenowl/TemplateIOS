a961ecbe1cfcfe08a5438f7a5b353ccee0e20819
paul-nguyen-goldenowl <paul.nguyen.goldenowl@gmail.com>
2022-12-27 17:51:20 +0700

    CI `xcworkspace`

--- TemplateIOS/ViewController.swift
+++ TemplateIOS/ViewController.swift
@@ -5,11 +5,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .blue
-
-        if true {
-            print("adsfnalkdjfnklasmfksamdfkljadk;lfjnkldfsdfsdfdsfsdfasdnf;klasndflkansdfosdjn;flkdaslfnmlsd")
-        }
-
-        let aa = [1, 2, 4, 45, 5, 5, 5, 5]
     }
 }
